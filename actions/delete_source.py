f86c7172d4cab120431457976e9a83addececc89
Igor Cherkaev <igor.cherkaev@copart.com>
2017-01-24 14:50:44 -0600

    Added delete source action

---
+++ actions/delete_source.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+from lib.actions import BaseAction
+
+
+class SumoDeleteSource(BaseAction):
+    def run(self, collector_id=None, source_id=None):
+
+        self.logger.debug('collector_id: %d', collector_id)
+        self.logger.debug('source_id: %d', source_id)
+
+        result = {}
+        if self._client is None:
+            self.logger.debug('No Access ID or Key is configured. Please, configure.')
+            result['status'] = 'No Access ID or Key is configured. Please, configure.'
+            return False, result
+
+        if collector_id is None:
+            self.logger.debug('No Collector ID was provided.')
+            result['status'] = 'Failed: No Collector ID was provided.'
+            return False, result
+
+        if source_id is None:
+            self.logger.debug('No source ID was provided.')
+            result['status'] = 'No source ID was provided.'
+            return False, result
+
+        # creating this stupid hash to pass it to delete_source method so it can extract the ID from it
+        data = { 'source': { 'id': source_id } }
+        res = self._client.delete_source(collector_id, data)
+        return True, res.json()
