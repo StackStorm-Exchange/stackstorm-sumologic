30853b351967c1dc25b9a7994dc10a83cf59328a
Igor Cherkaev <igor.cherkaev@copart.com>
2017-01-23 18:33:59 -0600

    Re-wrote get.id action

--- actions/get_id.py
+++ actions/get_id.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 from lib.actions import BaseAction
-from lib.mycollectors import MyCollectors
+import lib.utils
 
 
 class SumoGetId(BaseAction):
@@ -13,12 +13,6 @@ class SumoGetId(BaseAction):
             self.logger.debug('No Access ID or Key is configured. Please, configure.')
             return False, result
 
-        collector = MyCollectors(self._client)
-        c = collector.find_sub(keyword)
-        if c is not None:
-            ids = []
-            for i in c:
-                ids.append(i["id"])
-            return True, ids
-        else:
-            return True, []
+        collectors = self._client.collectors()
+        c = lib.utils.find_by_field(collectors, 'name', keyword, False)
+        return True, [d['id'] for d in c if 'id' in d]
