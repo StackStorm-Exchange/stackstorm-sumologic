882f48e0bb89ca294d398c3e089a7edf6c2690ad
Igor Cherkaev <igor.cherkaev@copart.com>
2017-01-24 15:00:58 -0600

    fixing searching collectors by id

--- actions/lib/utils.py
+++ actions/lib/utils.py
@@ -2,6 +2,8 @@ def find_by_field(cs, field, keyword, exact_match):
     res = []
     for c in cs:
         if exact_match:
+            if field == 'id':
+                keyword = int(keyword)
             if field in c and keyword == c[field]:
                 res.append(c)
         else:
